813f2a4201b2dea96696747aa443d2ebaac02eac
AdiDAgamer <61423116+AdiDAgamer@users.noreply.github.com>
2020-03-21 21:16:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,16 @@ var engine, world;
 
 var ground;
 var botPlayer;
+var object;
 
 function setup() 
 {
-  createCanvas(800, 800);
+  createCanvas(800, 600);
   engine = Engine.create();
-  world = engine.World;
-  ground = new Ground(400, 375, 800, 50);
-  botPlayer = new Player(100, 355, 25, 25);
+  world = engine.world;
+  ground = new Ground(400, 575, 800, 50);
+  botPlayer = new Player(100, 525, 25, 75);
+  object = new Object(700, 300, 40, 600);
 }
 
 function draw() 
@@ -21,4 +23,5 @@ function draw()
     
   ground.display();
   botPlayer.display();
+  object.display();
 }
